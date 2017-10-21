28caa1854b5855ab358ee16ab41ac2a89c040cb0
kelvin xie <kelvin xie>
2017-10-21 19:38:26 -0400

    debugging

--- cs144_lab3/router/sr_vns_comm.c
+++ cs144_lab3/router/sr_vns_comm.c
@@ -567,6 +567,7 @@ int sr_send_packet(struct sr_instance* sr /* borrowed */,
     c_packet_header *sr_pkt;
     unsigned int total_len =  len + (sizeof(c_packet_header));
 
+	print_hdrs(buf, len);
     /* REQUIRES */
     assert(sr);
     assert(buf);
