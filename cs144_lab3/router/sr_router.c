156c3c9504cd1135b1a8e19a2f30509ca96f0f45
kelvin xie <kelvin xie>
2017-10-21 18:36:33 -0400

    debugging

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -302,12 +302,12 @@ void sr_handlepacket(struct sr_instance* sr,
 			  retARPhdr = (sr_arp_hdr_t *) (reply + sizeof(sr_ethernet_hdr_t));
 			  /*Setup ethernet header*/
 			  memcpy(retEhdr->ether_shost, if_walker->addr, sizeof(uint8_t) * 6);
-			  memcpy(retEhdr->ether_dhost, ehdr->ether_shost, sizeof(uint8_t) * 6);
+			  memcpy(retEhdr->ether_dhost, ehdr->ether_dhost, sizeof(uint8_t) * 6);
 			  retEhdr->ether_type = htons(ethertype_arp);
 			  /*Setup ARP header*/
 			  memcpy(retARPhdr, arp_hdr, sizeof(sr_arp_hdr_t));
-			  memcpy(retARPhdr->ar_sha, if_walker->addr, sizeof(uint8_t) * 6);
-			  memcpy(retARPhdr->ar_tha, ehdr->ether_shost, sizeof(uint8_t) * 6);
+			  memcpy(retARPhdr->ar_sha, arp_hdr->ar_tha, sizeof(unsigned char) * 6);
+			  memcpy(retARPhdr->ar_tha, arp_hdr->ar_sha, sizeof(uint8_t) * 6);
 			  retARPhdr->ar_sip = if_walker->ip;
 			  retARPhdr->ar_tip = arp_hdr->ar_sip;
 			  retARPhdr->ar_op = htons(arp_op_reply);
