60a86b5347c23c3a9405d08520d8948a4f87894f
kelvin xie <kelvin xie>
2017-10-21 18:08:49 -0400

    debugging

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -301,7 +301,7 @@ void sr_handlepacket(struct sr_instance* sr,
 			  retEhdr = (sr_ethernet_hdr_t *) reply;
 			  retARPhdr = (sr_arp_hdr_t *) (reply + sizeof(sr_ethernet_hdr_t));
 			  /*Setup ethernet header*/
-			  memcpy(retEhdr->ether_shost, ehdr->ether_dhost, sizeof(uint8_t) * 6);
+			  memcpy(retEhdr->ether_shost, if_walker->addr, sizeof(uint8_t) * 6);
 			  memcpy(retEhdr->ether_dhost, ehdr->ether_shost, sizeof(uint8_t) * 6);
 			  /*Setup ARP header*/
 			  memcpy(retARPhdr, arp_hdr, sizeof(sr_arp_hdr_t));
