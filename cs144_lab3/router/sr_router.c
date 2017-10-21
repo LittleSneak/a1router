9f54ef1c4c40295174d55f25be66f9796b81da88
kelvin xie <kelvin xie>
2017-10-21 19:58:05 -0400

    debugging

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -156,6 +156,8 @@ void sr_handlepacket(struct sr_instance* sr,
 	  }
 	  /*Check if the message is an echo request*/
 	  if(type == 1 && found == 1){
+		  printf("here\n");
+	      print_hdrs(packet, len);
 		  icmp_hdr = (sr_icmp_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
 		  if(cksum(icmp_hdr, sizeof(sr_icmp_hdr_t)) != 0){
 			  return;
