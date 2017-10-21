c91d410b3bf93297ca79e699b6a85463ed253703
kelvin xie <kelvin xie>
2017-10-21 18:44:56 -0400

    debugging

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -142,13 +142,9 @@ void sr_handlepacket(struct sr_instance* sr,
   
   /*Handle IP packet or an ICMP packet*/
   if(type == 0 || type == 1){
-	  print_hdrs(packet, len);
-	  fflush(stdout);
 	  /*Obtain ip header*/
 	  iphdr = (sr_ip_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t));
 	  /*Check the checksum*/
-	  print_hdrs(packet, len);
-	  fflush(stdout);
 	  if(cksum(iphdr, sizeof(sr_ip_hdr_t)) != 0){
 		  return;
 	  }
