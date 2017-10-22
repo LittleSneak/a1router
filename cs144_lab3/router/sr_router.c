d299fde7b6563f3d806880642778113bb67e64ec
kelvin xie <kelvin xie>
2017-10-21 23:31:14 -0400

    testing arp cache

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -384,6 +384,11 @@ void sr_handlepacket(struct sr_instance* sr,
 			  while(req_walker != NULL){
 				  ehdr = (sr_ethernet_hdr_t *) req_walker->buf;
 				  memcpy(ehdr->ether_dhost, arp_hdr->ar_sha, sizeof(ehdr->ether_dhost));
+				  printf("before sending\n");
+			      fflush(stdout);
+				  sleep(0.1);
+				  printf("SENDING NOW\n");
+			      fflush(stdout);
 				  sr_send_packet(sr /* borrowed */,
                          req_walker->buf /* borrowed */ ,
                          req_walker->len,
