d7a2f433c104f2a2e65885aa3f194eb878c53540
kelvin xie <kelvin xie>
2017-11-25 17:54:47 -0500

    a2

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -748,6 +748,7 @@ void sr_handle_nat(struct sr_instance* sr, uint8_t *packet, unsigned int len, ch
 			tcphdr->checksum = cksum(tcphdr, len - sizeof(sr_ethernet_hdr_t) - sizeof(sr_ip_hdr_t));
 		}
 		memcpy(ehdr->ether_shost, ext_if->addr, sizeof(uint8_t) * 6);
+	    memcpy(ehdr->ether_dhost, ext_if->addr, sizeof(uint8_t) * 6);
 		print_hdrs(packet, len);
 		iphdr->ip_src = ext_if->ip;
 		iphdr->ip_sum = 0;
