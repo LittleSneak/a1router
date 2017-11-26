917fcf5d3ec26c16e38aa7fac77be4004097414a
kelvin xie <kelvin xie>
2017-11-25 21:31:59 -0500

    test

--- cs144_lab3/router/sr_nat.c
+++ cs144_lab3/router/sr_nat.c
@@ -294,15 +294,15 @@ struct sr_nat_mapping *sr_nat_insert_mapping(struct sr_nat *nat,
 struct sr_nat_connection * sr_nat_insert_connection(struct sr_nat *nat, uint32_t ip_int, uint16_t aux_int, uint32_t ip){
 	pthread_mutex_lock(&(nat->lock));
 	struct *sr_nat_mapping mapping = nat->mappings;
+	struct sr_nat_connection *newConn = (struct sr_nat_connection *) malloc(sizeof(struct sr_nat_connection));
 	while(mapping != NULL){
 		if(mapping->type == nat_mapping_tcp && mapping->aux_int == aux_int && mapping->ip_int == ip_int){
 		    break;
 	    }
     }
 	if (mapping == NULL){
-		return;
+		return newConn;
 	}
-	struct sr_nat_connection *newConn = (struct sr_nat_connection *) malloc(sizeof(struct sr_nat_connection));
 	newConn->ip = ip;
 	newConn->last_updated = time(NULL);
 	newConn->state = tcp_listen;
